379db94a25c5088f8335e8e20c88470328f2a8e9
SamJohnson02 <144283303+SamJohnson02@users.noreply.github.com>
2024-09-19 18:10:21 -0500

    lots of changes
    removes Disney and amazon.
    Added images, used chrome storage to remember choice. zdded popup. and more.

--- netflix.js
+++ netflix.js
@@ -1,25 +1,23 @@
-function skip() {
-  // recap button
-  const recapButton =  document.querySelector("#appMountPoint > div > div > div:nth-child(1) > div > div > div.passive.default-ltr-cache-fntwn3 > div.default-ltr-cache-1m81c36 > div.watch-video--skip-content.default-ltr-cache-gpipej > button")
+let intervalId = null;
 
-  // skip button 
-  const introButton = document.querySelector("#appMountPoint > div > div > div:nth-child(1) > div > div > div.active.default-ltr-cache-fntwn3 > div.default-ltr-cache-1m81c36 > div.watch-video--skip-content.default-ltr-cache-gpipej > button")
+function skip(options) {
 
-  // next episode
-  const nextEpisode = document.querySelector("#appMountPoint > div > div > div:nth-child(1) > div > div > div.active.default-ltr-cache-fntwn3 > div.nfa-pos-abs.nfa-top-0.nfa-left-0.nfa-right-0.nfa-bot-0.nfa-d-flex.nfa-ai-flex-end.nfa-jc-flex-end.SeamlessControls--container.SeamlessControls--container-visible > div.nfa-pos-abs.nfa-bot-6-em.nfa-right-5-em.nfa-d-flex > button.color-primary.hasLabel.hasIcon.default-ltr-cache-18ezbm2")
-
-  if (recapButton) {
-    recapButton.click();
-  } 
-  if (introButton) {
-    introButton.click();
-  }
-  if (nextEpisode) {
-    nextEpisode.click();
+  if (options.skipIntro) {
+    const introButton = document.querySelector("#appMountPoint > div > div > div:nth-child(1) > div > div > div.active.default-ltr-cache-fntwn3 > div.default-ltr-cache-1m81c36 > div.watch-video--skip-content.default-ltr-cache-gpipej > button");
+    if (introButton) {
+      introButton.click();
+    }
   }
 }
 
-setInterval(function() {
-  skip();  
-}, 1000);  
+chrome.runtime.onMessage.addListener((message) => {
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
 
+  if (message.skipIntro) {
+    intervalId = setInterval(() => {
+      skip(message);
+    }, 1000);  // Check every second
+  }
+});
