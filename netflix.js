3dbd5ab89233e591a5894468b3f5ecb71fa0f348
SamJohnson02 <144283303+SamJohnson02@users.noreply.github.com>
2024-09-24 17:38:30 -0500

    Simplified, removed storage req
    got rid of popup.js, simplified it so it works better

--- netflix.js
+++ netflix.js
@@ -1,23 +1,14 @@
-let intervalId = null;
 
-function skip(options) {
+function skip() {
 
-  if (options.skipIntro) {
-    const introButton = document.querySelector("#appMountPoint > div > div > div:nth-child(1) > div > div > div.active.default-ltr-cache-fntwn3 > div.default-ltr-cache-1m81c36 > div.watch-video--skip-content.default-ltr-cache-gpipej > button");
-    if (introButton) {
-      introButton.click();
-    }
+  const introButton = document.querySelector("#appMountPoint > div > div > div:nth-child(1) > div > div > div.active.default-ltr-cache-fntwn3 > div.default-ltr-cache-1m81c36 > div.watch-video--skip-content.default-ltr-cache-gpipej > button")
+
+  if (introButton) {
+    introButton.click();
   }
 }
+setInterval(function() {
+  skip();  
+}, 1000);  
 
-chrome.runtime.onMessage.addListener((message) => {
-  if (intervalId) {
-    clearInterval(intervalId);
-  }
-
-  if (message.skipIntro) {
-    intervalId = setInterval(() => {
-      skip(message);
-    }, 1000);  // Check every second
-  }
-});
+/* div > div.watch-video--skip-content.default-ltr-cache-gpipej > button */
